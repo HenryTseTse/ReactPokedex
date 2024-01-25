fc2c955b0824b05c68bc495ab43a7cdc23c2ceec
Henry Tse <ht688@nyu.edu>
2024-01-25 14:43:14 -0500

    final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import pokemons from "./pokemons";
+import Pokedex from "./Pokedex"
+
+function App() {
+  return (
+    <div className="App">
+      <Pokedex pokemons={pokemons} />
+    </div>
+  );
+}
+
+export default App;
